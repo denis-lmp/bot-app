b7e6e1e87479bbca9ecc0a98778a91774fa086dc
dev-kostaev <denis.kostaev@lamp-dev.com>
2024-01-10 12:53:22 +0100

    change to any

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/bot/getOrders', [CryptoTradingController::class, 'botCallback']);
+Route::any('/bot/getOrders', [CryptoTradingController::class, 'botCallback']);
