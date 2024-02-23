d081be8fde8e648e1d19186e5db36690da3ea8ce
dev-kostaev <denis.kostaev@lamp-dev.com>
2024-02-23 12:04:59 +0100

    add registration route

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,9 +6,14 @@ use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+    /**
+     * @param  Request  $request
+     * @return JsonResponse
+     */
     public function login(Request $request): JsonResponse
     {
         if (!Auth::attempt($request->only('email', 'password'))) {
@@ -24,6 +29,42 @@ class AuthController extends Controller
         return response()->json([
             'access_token' => $token,
             'token_type'   => 'Bearer',
+        ]);
+    }
+
+    /**
+     * @param  Request  $request
+     * @return JsonResponse
+     */
+    public function register(Request $request): JsonResponse
+    {
+        $registerUserData = $request->validate([
+            'name'                  => 'required|string',
+            'email'                 => 'required|string|email|unique:users',
+            'password'              => 'required|min:8',
+            'password_confirmation' => 'required|same:password'
+        ]);
+
+        $user = User::create([
+            'name'     => $registerUserData['name'],
+            'email'    => $registerUserData['email'],
+            'password' => Hash::make($registerUserData['password']),
+        ]);
+
+        return response()->json([
+            'message' => 'User Created ',
+        ]);
+    }
+
+    /**
+     * @return JsonResponse
+     */
+    public function logout()
+    {
+        auth()->user()->tokens()->delete();
+
+        return response()->json([
+            "message" => "logged out"
         ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -21,10 +21,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
 Route::post('/login', [AuthController::class, 'login']);
-Route::get('/orders', [CryptoTradingController::class, 'botCallback']);
+Route::post('/register',[AuthController::class,'register']);
+Route::post('/logout',[AuthController::class,'logout'])
+    ->middleware('auth:sanctum');
 
+Route::get('/orders', [CryptoTradingController::class, 'botCallback']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/prices', [PriceController::class, 'getPrices']);
